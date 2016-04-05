2f2c763e3700c5ba977c6ef27e67f254ebfc670b
Tanner Garrett <tg71223@uw.edu>
2016-04-05 16:53:06 -0700

    Add all functionality includingaveraging, counting, and computing factorials

---
+++ README.md
@@ -0,0 +1,6 @@
+ Author:        Tanner Garrett
+ Date:          4/5/2016
+ Class:         Informatics 498 D
+ Assignment:    SimpleCalc
+
+ This program allows a user to input commands in order to do simple calculations such as addition, multiplication subtraction and modulation. It also supports averaging, counting, and computing factorials. All operations except factorial support the input of doubles and negative numbers. The program will continue to cycle until the user specifies they do not want to continue playing.
--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -2,38 +2,54 @@
 //  main.swift
 //  SimpleCalc
 //
-//  Created by iGuest on 4/5/16.
+//  Created by Tanner on 4/5/16.
 //  Copyright © 2016 Tanner. All rights reserved.
 //
+//  This program allows a user to input commands in order to do simple operations such as addition, multiplication
+//  subtraction and modulation. It also supports averaging, counting, and computing factorials. All operations except
+//  factorial support the input of doubles and negative numbers. The program will continue to cycle until the user
+//  specifies they do not want to continue playing.
 
 import Foundation
 
-/*
-*
-*
-*
-*/
-func input() -> String{
-    let keyboard = NSFileHandle.fileHandleWithStandardInput();
-    let inputData = keyboard.availableData;
-    return NSString(data: inputData, encoding: NSUTF8StringEncoding) as! String;
-}
-
-func computation() {
+// Contains logic that prints out computations and instructions depending on the input the
+// user provides. Returns True if the user wants to continue inputting computations and
+// False if they want to terminate the program
+func computation() -> Bool {
     print("Please Insert Your Computation :");
     let firstPassUI = input();
-    var firstPassArray = firstPassUI.characters.split{$0 == " "}.map(String.init);
-    print(firstPassUI);
-    
+    let firstPassArray = firstPassUI.characters.split{$0 == " "}.map(String.init);
+    let firstPassEndIndex = removeNewline((firstPassArray.last)!);
     if(firstPassArray.count == 1) {
-        //Take the new line char off of the value
         let trimmedDouble = removeNewline(firstPassArray[0]);
-        //print("\(trimmedDouble)");
         let doubleString:Double? = Double(trimmedDouble);
         operationComputation(doubleString!);
+    // Test and Logic for count
+    } else if (firstPassEndIndex.lowercaseString == "count") {
+        print("The count is : \(firstPassArray.count - 1)");
+    } else if (firstPassEndIndex.lowercaseString == "avg") {
+        let avgRange = 0...firstPassArray.count-2;
+        var sum = 0.0;
+        for indexNumber in avgRange {
+            let doubleString:Double! = Double(firstPassArray[indexNumber]);
+            sum += doubleString;
+        }
+        print("The average is :  \(sum / Double(avgRange.count - 1))");
+    } else if (firstPassEndIndex.lowercaseString == "fact") {
+        if(firstPassArray.count != 2) {
+            print("Please format a factorial call with the given format : '[Integer] fact'");
+        } else {
+            print("\(factorial(Int(firstPassArray[0])!))");
+        }
+
+    } else {
+        print("You put in an invalid query");
     }
+    return continueQuery();
 }
 
+// Contains the logic for simple addition, subtraction, division, multiplication,
+// and modulation - printing out information to the user as is appropriate
 func operationComputation(numberOne: Double) {
     print("Please add an Operator :");
     let operatorString = removeNewline(input());
@@ -43,6 +59,8 @@ func operationComputation(numberOne: Double) {
         print("\(numberOne) + \(numberTwo!) = \(numberOne + numberTwo!)");
     } else if (operatorString == "-") {
         print("\(numberOne) - \(numberTwo!) = \(numberOne - numberTwo!)");
+    } else if (operatorString == "*") {
+        print("\(numberOne) * \(numberTwo!) = \(numberOne * numberTwo!)");
     } else if (operatorString == "/") {
         print("\(numberOne) / \(numberTwo!) = \(numberOne / numberTwo!)");
     } else if (operatorString == "%") {
@@ -52,6 +70,8 @@ func operationComputation(numberOne: Double) {
     }
 }
 
+// Removes the last two characters of whatever String is passed in, returning the resulting
+// String
 func removeNewline(newlineString: String) -> String {
     var trimmedString = newlineString;
     let range = trimmedString.endIndex.advancedBy(-1)..<trimmedString.endIndex;
@@ -59,22 +79,43 @@ func removeNewline(newlineString: String) -> String {
     return trimmedString;
 }
 
-computation();
-//print("Please Type in Your Name :");
-//
-//var userInput = input();
-//
-//let firstPassUI = userInput.characters.split{$0 == " "}.map(String.init);
-//firstPassUI.last
-//
-//let text = "text"
-//
-//    
-//print("\(userInput)");
-//
-//var herpInput = input();
-//
-//print("\(herpInput)");
-//
-//print(firstPassUI);
+// Given an Int, returns an Int that is the result of a factorial operation on the Int 
+// passed in
+func factorial(factInt: Int) -> Int {
+    if(factInt <= 1) {
+        return 1;
+    } else {
+        return factInt * factorial(factInt - 1);
+    }
+}
+
+// Queries the user on whether or not they want to complete another operation. Returns True
+// if they respond with yes and False if they respond with no
+func continueQuery() -> Bool {
+    print("Would you like to make another query?");
+    var continueAnswer = "";
+    // Continue asking the user if they want to make another query until they answer "yes" or "no"
+    while (continueAnswer.lowercaseString != "yes" && continueAnswer.lowercaseString != "no") {
+        print("Please answer 'Yes' or 'No'");
+        continueAnswer = removeNewline(input());
+    }
+    return (continueAnswer.lowercaseString == "yes");
+    
+}
+
+// Returns an NSString depending on what the user inputs using the keyboard
+func input() -> String{
+    let keyboard = NSFileHandle.fileHandleWithStandardInput();
+    let inputData = keyboard.availableData;
+    return NSString(data: inputData, encoding: NSUTF8StringEncoding) as! String;
+}
+
+// Starts the cycle of computations, and terminates once the user decides they want to stop
+
+var cycle = true;
+
+while cycle {
+    cycle = computation();
+}
+
 
